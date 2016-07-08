f6a2d2fc09e258e0b3f918870c2a3c3363899cdc
Alex <antis@DESKTOP-B3PM68I>
2016-07-08 14:52:18 +0200

    Izmenjen meni bar dodate funkcije

--- AAA/src/Controler/menuBarActions.py
+++ AAA/src/Controler/menuBarActions.py
@@ -1,5 +1,17 @@
-'''from PySide.QtGui import QAction,QIcon
+from PySide.QtGui import QAction,QIcon
 
 class menuBarActions():
     def mBarActions(self):
-        '''
+        self.newFileAction = QAction(QIcon('Icons/new.png'),"New",self)
+        self.openFileAction = QAction(QIcon('Icons/new.png'),"Open",self)
+        self.saveAction = QAction(QIcon('Icons/new.png'),"Save",self)
+        self.saveAsAction = QAction(QIcon('Icons/new.png'),"Save As",self)
+        self.exitProgramAction = QAction(QIcon('Icons/new.png'),"Exit",self)
+        
+        self.undoAction = QAction(QIcon('Icons/new.png'),"Undo",self)
+        self.redoAction = QAction(QIcon('Icons/new.png'),"Redo",self)
+        self.cutAction = QAction(QIcon('Icons/new.png'),"Cut",self)
+        self.copyAction = QAction(QIcon('Icons/new.png'),"Copy",self)
+        self.pasteAction = QAction(QIcon('Icons/new.png'),"Paste",self)
+        
+        self.optionsAction = QAction(QIcon('Icons/new.png'),"Options",self)
--- AAA/src/Viewer/menuBar.py
+++ AAA/src/Viewer/menuBar.py
@@ -1,16 +1,37 @@
 from PySide.QtGui import QMenu ,QMenuBar
-#from Controler import menuBarActions
+from Controler import menuBarActions
 class menuBar(QMenuBar):
     def __init__(self):
-        #menuBarActions.menuBarActions.mBarActions(self)
+        
         QMenuBar.__init__(self)
+        
+        menuBarActions.menuBarActions.mBarActions(self)
+        #File
         self.fileMenu = QMenu()
         self.fileMenu.setTitle("File")
-        self.fileNewMenu = QMenu()
-        self.fileNewMenu.setTitle("New")
-        #fileNewMenu.addAction(self.newTxtAction)
-        self.fileMenu.addMenu(self.fileNewMenu)
+        self.fileMenu.addAction(self.newFileAction)
         self.fileMenu.addSeparator()
-        
+        self.fileMenu.addAction(self.openFileAction)
+        self.fileMenu.addSeparator()
+        self.fileMenu.addAction(self.saveAction)
+        self.fileMenu.addSeparator()
+        self.fileMenu.addAction(self.saveAsAction)
+        self.fileMenu.addSeparator()
+        self.fileMenu.addAction(self.exitProgramAction)
         self.addMenu(self.fileMenu)
-        
+        #Edit
+        self.editMenu=QMenu()
+        self.editMenu.setTitle("Edit")
+        self.editMenu.addAction(self.undoAction)
+        self.fileMenu.addSeparator()
+        self.editMenu.addAction(self.redoAction)
+        self.fileMenu.addSeparator()
+        self.editMenu.addAction(self.cutAction)
+        self.fileMenu.addSeparator()
+        self.editMenu.addAction(self.copyAction)
+        self.fileMenu.addSeparator()
+        self.editMenu.addAction(self.pasteAction)
+        self.fileMenu.addSeparator()
+        self.addMenu(self.editMenu)
+
+        self.addAction(self.optionsAction)
